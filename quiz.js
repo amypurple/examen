e1918359aae20d2bd66dc24b5801fe371f3d143d
Amy Bienvenu <amy.bienvenu@outlook.com>
2022-03-24 21:33:51 -0400

    firt commit

---
+++ quiz.js
@@ -0,0 +1,173 @@
+const HTMLnumero = document.getElementById("numero");
+const HTMLpoints = document.getElementById("points");
+const HTMLtotal = document.getElementById("total");
+const HTMLquestion = document.getElementById("question");
+const HTMLchoix = document.getElementById("choix");
+
+var points = 0;
+var numero = 0;
+var total = 0; 
+
+var clickable = true;
+
+function startQuiz() {
+    numero = -1;
+    points = 0;
+    total = data.length;
+    HTMLtotal.innerHTML = total;
+    loadNextQuestion();
+}
+
+function loadNextQuestion() {
+    numero++;
+    if (numero<total) {
+        HTMLnumero.innerHTML = (numero+1)+".";
+        HTMLpoints.innerHTML = points;
+        HTMLquestion.innerHTML = data[numero].question;
+
+        let choix = data[numero].choix;
+        let reponse = data[numero].reponse;
+
+        HTMLchoix.innerHTML = "";
+
+        let i;
+        for (i=0;i<choix.length;i++) {
+            let div = document.createElement("div");
+            div.innerHTML = choix[i];
+            div.classList.add("choix");
+            if (reponse == i+1) {    
+                div.addEventListener("click", (e) => {
+                    if (clickable)
+                    {
+                        e.currentTarget.classList.add("good");
+                        points++;
+                        HTMLpoints.innerHTML = points;
+                        clickable = false;
+                    } else {
+                        loadNextQuestion();
+                    }
+                });
+            } else {
+                div.addEventListener("click", (e) => {
+                    if (clickable)
+                    {
+                        e.currentTarget.classList.add("bad");
+                        clickable = false;
+                    } else {
+                        loadNextQuestion();
+                    }
+                });
+            }
+            HTMLchoix.appendChild(div);
+        }
+
+        clickable = true;
+    } else {
+        alert("Quiz Terminé");
+    }
+}
+
+
+var data = [
+    {
+        "question":"Vrai ou faux ? La phase de conception permet de spécifier dans les grandes lignes comment l'application Web doit se comporter tel que perçu par l'utilisateur",
+        "choix": [
+            "Vrai",
+            "Faux"
+        ],
+        "reponse": 1
+    },
+    {
+        "question":"Que signifie « modéliser » un logiciel",
+        "choix":[
+            "Réalisation des tests de validation d’un logiciel",
+            "Représentation à l’aide de différents diagrammes",
+            "Écriture de l’algorithme d’un logiciel",
+            "Toutes ces réponses"],
+        "reponse": 2
+    },
+    {
+        "question":"Qu'est ce que le contexte d'un système ou logiciel",
+        "choix": ["Les systèmes en relation avec le système","Les relations du logiciel","C’est l’environnement direct du logiciel","Les cas d'utilisation du système","Aucune de ces réponses"],
+        "reponse": 3
+    },
+    {
+        "question":"Quel est la relation entre deux cas d'utilisation qui indique que le second cas d'utilisation est toujours exécuté",
+        "choix": [
+            "Exclude",
+            "Include",
+            "Extend",
+            "Association",
+            "Aucune de ces réponses"
+        ],
+        "reponse": 2
+    },
+    {
+        "question":"Qui suis-je ? Je suis considéré comme un référentiel partagé par un prestataire de service et l’équipe interne.",
+        "choix": [
+            "Cahier de production",
+            "Diagramme de cas d'utilisation",
+            "UML",
+            "Cahier des charges",
+            "Aucune de ces réponses"
+        ],
+        "reponse": 4
+    },
+    {
+        "question":"Qui suis-je ? Une collection d’activité pour produire un résultat pour un client ou un marché.",
+        "choix":[
+            "Un cas d'utilisation",
+            "Diagramme de classe",
+            "Business Process Modeling Notation",
+            "Processus métier",
+            "Aucune de ces réponses"
+        ],
+        "reponse": 4
+    },
+    {
+        "question":"Qu'est ce que UML",
+        "choix": [
+            "Un langage de modélisation",
+            "Un langage de développement objet",
+            "Une démarche de définition de logiciel",
+            "Une méthode de gestion de projet",
+            "Aucune de ces réponses"
+        ],
+        "reponse": 1
+    },
+    {
+        "question":"De combien de diagrammes se compose UML",
+        "choix": [
+            "4 + 1 diagrammes",
+            "9 diagrammes",
+            "Une vingtaine de diagrammes",
+            "14 diagrammes",
+            "Aucune des ces réponses"
+        ],
+        "reponse": 4
+    },
+    {
+        "question":"Dans quel axe de la phase conception que nous découpons les données en entités-relations",
+        "choix":[
+            "Travail",
+            "Fonctions",
+            "Information",
+            "Architecture de l'intreface",
+            "Aucune de ces réponses"
+        ],
+        "reponse": 3
+    },
+    {
+        "question":"Dans quel phase est surtout utilisée le diagramme de cas d'utilisation",
+        "choix": [
+            "Phase Analyse",
+            "Phase Conception",
+            "Phase Réalisation",
+            "Phase déploiement",
+            "Aucune des ces réponses"
+        ],
+        "reponse": 1
+    }
+];
+
+startQuiz();
